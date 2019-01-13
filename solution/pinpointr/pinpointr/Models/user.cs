a36148de6c586e07dabd2226b2730f07e051ccbb
kippmr <kippmr@gmail.com>
2019-01-12 21:41:14 -0500

    create models and initial migrations

---
+++ solution/pinpointr/pinpointr/Models/user.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pinpointr.Models
+{
+    public class user
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public int student_id { get; set; }
+        public int employee_id { get; set; }
+        public string email { get; set; }
+    }
+}
--- solution/pinpointr/pinpointr/Startup.cs
+++ solution/pinpointr/pinpointr/Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using pinpointr.Models;
 
 namespace pinpointr
 {
@@ -20,6 +22,10 @@ namespace pinpointr
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
+            services.AddDbContext<RDSContext>(options =>
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the React files will be served from this directory
