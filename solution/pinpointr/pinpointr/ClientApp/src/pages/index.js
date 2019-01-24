fa086e4f176b527bc6387e90a9725ae280bdbd00
Victor Timpau <timpauvictor@gmail.com>
2019-01-24 12:26:02 -0500

    added buggy map and a persistent nvabar

--- solution/pinpointr/pinpointr/ClientApp/src/pages/index.js
+++ solution/pinpointr/pinpointr/ClientApp/src/pages/index.js
@@ -19,6 +19,8 @@ import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 
+import LoadLeaflet from './leafletEx';
+
 const drawerWidth = 240;
 
 const styles = theme => ({
@@ -138,45 +140,13 @@ class persistentNavbar extends React.Component {
                     </div>
                     <Divider />
                     <List>
-
-                    </List>
-                    <Divider />
-                    <List>
                         <ListItem button key='Stats'>
                             <ListItemText primary='Stats' />
                         </ListItem>
                     </List>
                 </Drawer>
-                <main
-                    className={classNames(classes.content, {
-                        [classes.contentShift]: open,
-                    })}
-                >
-                    <div className={classes.drawerHeader} />
-                    <Typography paragraph>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
-                        elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
-                        hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
-                        velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
-                        Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
-                        viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
-                        Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
-                        at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
-                        ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-          </Typography>
-                    <Typography paragraph>
-                        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-                        facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-                        tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-                        consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus
-                        sed vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in.
-                        In hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                        et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique
-                        sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo
-                        viverra maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
-                        ultrices sagittis orci a.
-          </Typography>
+                <main>
+                    <LoadLeaflet/>
                 </main>
             </div>
         );
---
+++ solution/pinpointr/pinpointr/ClientApp/src/pages/leafletEx.js
@@ -0,0 +1,35 @@
+﻿import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { Map, TileLayer } from 'react-leaflet';
+const stamenTonerTiles = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png';
+const stamenTonerAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+const mapCenter = [39.9528, -75.1638];
+const zoomLevel = 12;
+
+const styles = {
+    root: {
+        width: '100%',
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 700,
+    },
+};
+
+function LoadLeaflet() {
+        return (
+            <div>
+                <Map
+                    center={mapCenter}
+                    zoom={zoomLevel}
+                >
+                    <TileLayer
+                        attribution={stamenTonerAttr}
+                        url={stamenTonerTiles}
+                    />
+                </Map>
+            </div>
+        );
+}
+
+export default withStyles(styles)(LoadLeaflet);
