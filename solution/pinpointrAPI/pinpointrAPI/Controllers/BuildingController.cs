841bdbce95b8e7fa9bf91574bad2bc45d3ebada9
Victor Timpau <timpauvictor@gmail.com>
2019-04-24 16:15:03 -0400

    progress

---
+++ solution/pinpointrAPI/pinpointrAPI/Controllers/BuildingController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using pinpointrAPI.Models;
+using pinpointrAPI.Helpers;
+using System.Net;
+using System.Net.Mail;
+
+namespace pinpointrAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuildingController
+    {
+
+        List<PointBuilding> pointBuildings = new List<PointBuilding>();
+
+        public BuildingController()
+        {
+            string[] buildingFiles = Directory.GetFiles("./Helpers/Buildings/", "*.txt", SearchOption.AllDirectories);
+            
+
+            foreach (string s in buildingFiles)
+            {
+                pointBuildings.Add(new PointBuilding(s));
+            }
+        }
+
+        // POST /api/Building
+        [HttpPost]
+        public ActionResult<string> Post(string xCoord, string yCoord)
+        {
+            return xCoord + 
+
+            foreach (PointBuilding pb in pointBuildings)
+            {
+                if (pb.collidesWith(43.258517, -79.92011))
+                {
+                    return pb.getName();
+                }
+            }
+            return null;
+        }
+    }
+}
